a809de7afef7332c30136c7be270bb12f76522db
vikrampatil1 <vicky.patil@yahoo.co.in>
2016-03-31 09:01:35 -0400

    Added files via upload

--- AES/Driver.java
+++ AES/Driver.java
@@ -1,17 +1,46 @@
- /* Author : Vikram Patil
- * Date : 23 Feb 2016
- * Description: Security Algorithm assignment
-*/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package AES;
+
+/**
+ *
+ * @author ek10
+ */
 import java.util.Scanner;
 
 public class Driver {
     public static void main(String args[]){    
            
-             Scanner sc = new Scanner(System.in);
-	     String input = sc.nextLine();	    
-	     AESKeyGenerator kg = new AESKeyGenerator();
-	     kg.generateRoundKeys(input);
-             
+             //Takes input from user and stores in inputkey
+    Scanner in1 = new Scanner(System.in);
+	String inputKey = "";
+	String plainText = "";
+    while (in1.hasNextLine())
+{
+    inputKey = in1.nextLine();
+    plainText = in1.nextLine();
+}
+    
+    //Takes 2nd input from user and stores in inputPlainText
+    //Scanner in2 = new Scanner(System.in);
+    //String plainText = in2.nextLine();
+    //If condition makes sure that user has entered exactly 32 hexadecimal digits
+    if ( (inputKey.matches("[0-9A-F]{32}") ) && (plainText.matches("[0-9A-F]{32}"))) {      
+      //cText stores the ciphertext
+      AEScipher kg = new AEScipher();
+      String cText = kg.aes(plainText, inputKey);
+      
+      //Print the ciphertext
+      System.out.print(cText);
     }
+      else {
+      //If user input is incorrect, terminate the program
+      System.out.println("Invalid input key or plaintext, exiting.....");
+    }
+                         
+}
     
 }
